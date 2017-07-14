d148219aa8d30c94a7aa606f49863a2b7b86abfa
Riktheking <ly196616105@gmail.com>
2017-07-14 11:19:40 -0400

    calculate PI

---
+++ bellard.py
@@ -0,0 +1,18 @@
+
+from decimal import *
+class Value:
+    def bellardBig(self,n): #http://en.wikipedia.org/wiki/Bellard%27s_formula
+        pi = Decimal(0)
+        k = 0
+        while k < n:
+            pi += (Decimal(-1)**k/(1024**k))*( Decimal(256)/(10*k+1) + Decimal(1)/(10*k+9) - Decimal(64)/(10*k+3) - Decimal(32)/(4*k+1) - Decimal(4)/(10*k+5) - Decimal(4)/(10*k+7) -Decimal(1)/(4*k+3))
+            k += 1
+        pi = pi * 1/(2**6)
+        print(self)
+        return pi
+
+if __name__=="__main__":
+    instan=Value();
+    x=instan.bellardBig(2);
+    print ("3.141765873015873015873015873");
+    print(x)
--- pi.py
+++ pi.py
@@ -1,15 +1,17 @@
 import sys
 import math
+"the class the calculate pi"
+class pi:
+    def main(argv):
 
-def main(argv):
+        if len(argv)!=1:
+            sys.exit('Usage:calc_pi_py')
 
-    if len(argv)!=1:
-        sys.exit('Usage:calc_pi_py')
 
-        a=1.0
-        b=1.0/math.sqrt(2)
-        t=1.0/4.0
-        p=1.0
+            a=1.0
+            b=1.0/math.sqrt(2)
+            t=1.0/4.0
+            p=1.0
 
         for i in range(int(sys.argv[1])):
             at=(a+b)/2
@@ -23,9 +25,11 @@ def main(argv):
             p=pt
 
             mypi=(a+b)**2/(4*t)
-
+            accuracy=100*(Decimal(math.pi)-my_pi)/my_pi
             print(str(mypi))
+            print(str(accuracy))
 
 
 if __name__=="__main__":
-    main(sys.argv[1:])
+    pi1=pi()
+    pi1.main(sys.argv[0])
